45e16c6e5d596f3457373c61703616b2f4be2365
malik.fleury@he-arc.ch <malik.fleury@he-arc.ch>
2019-03-24 17:14:16 +0100

    Create motion detection class

---
+++ Program/Imagery/MotionDetector.py
@@ -0,0 +1,68 @@
+
+import cv2
+import imutils
+import time
+from .Camera import *
+
+class MotionDetector:
+    def __init__(self, min_threshold = 15, max_threshold = 255, blur_matrix_size = (9, 9)):
+        self.min_threshold = min_threshold
+        self.max_threshold = max_threshold
+        self.blur_matrix_size = blur_matrix_size
+        self.last_frame = None
+
+    def process(self, current_frame):
+        last_frame = self.last_frame
+        # Tranformations
+        current_frame = imutils.resize(current_frame, width=500)
+        gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, self.blur_matrix_size, 0)
+
+        # Get the very first image and pass the first iteration
+        if last_frame is None:
+            last_frame = gray
+        else:
+            # Get difference between two images and then try to determine the threshold
+            frameDelta = cv2.absdiff(last_frame, gray)
+            threshold = cv2.threshold(frameDelta, self.min_threshold, self.max_threshold, cv2.THRESH_BINARY)[1]
+
+        	# dilate the thresholded image to fill in holes, then find contours
+        	# on thresholded image
+            thresh = cv2.dilate(threshold, None, iterations=2)
+            contours = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(contours)
+
+            for contour in contours:
+            	# compute the bounding box for the contour, draw it on the frame,
+            	# and update the text
+            	(x, y, w, h) = cv2.boundingRect(contour)
+            	cv2.rectangle(current_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            last_frame = gray
+        self.last_frame = last_frame
+
+        return current_frame
+
+    def process_and_convert_jpeg(self, current_frame):
+        current_frame = self.process(current_frame)
+        return cv2.imencode('.jpg', current_frame)[1].tobytes()
+
+def execute_test():
+    ''' Launch test : check if MotionDetector work as expected '''
+    camera = WebCamera()
+    motion_detector = MotionDetector()
+    last_frame = None
+    while True:
+        current_frame = camera.get_frame()[1]
+        processed_frame = motion_detector.process(current_frame)
+
+        cv2.imshow("Processed frame", processed_frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+        	break
+    cv2.destroyAllWindows()
+    camera.stop()
+
+if __name__ == "__main__":
+    execute_test()
